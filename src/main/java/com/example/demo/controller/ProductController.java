6d8ca7b2771b2a3877bbc1007162f2100d0f7b66
yaelsanch115 <yaelsanch1987@gmail.com>
2026-05-25 01:28:46 -0600

    Update ProductController.java

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -4,6 +4,8 @@ import com.example.demo.model.Product;
 import com.example.demo.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
@@ -11,11 +13,17 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
+
     @Autowired
     private ProductService service;
 
     @GetMapping
     public List<Product> listProducts() {
         return service.getAll();
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return service.save(product);
     }
 }
